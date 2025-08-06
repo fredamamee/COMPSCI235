c8cb28d1cb1fae771f1a30db976882f79044dbbd
fredamamee <chikeif@gmail.com>
2025-08-06 12:58:35 +1200

    Second commit yippee

--- category.py
+++ category.py
@@ -1,5 +1,5 @@
 class Category:
-    def __init__(self, idd, name, recipes):
+    def __init__(self, idd, name, poo):
         self.__name = name
         self.__id = idd
-        self.__recipes = recipes
+        self.__recipes = poo
